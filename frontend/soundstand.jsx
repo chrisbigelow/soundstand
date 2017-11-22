bb7456bbcb09702549206094f59defd44501863d
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-22 07:25:28 -0800

    Add app and root

--- frontend/soundstand.jsx
+++ frontend/soundstand.jsx
@@ -1,14 +1,11 @@
 import configureStore from './store/store'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, logout, signup} from './util/session_api_util';
+import Root from './components/Root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>SoundStand Root</h1>, root);
-
   const store = configureStore();
- 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  ReactDOM.render(<Root store={ store }/>, root);
 });

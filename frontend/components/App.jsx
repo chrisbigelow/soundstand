9a2268aaabec9d73c17deb0c074b4638650fbff1
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-22 10:11:12 -0800

    Add navbar

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignupContainer from './session/signup_container';
+import NavbarContainer from './navbar/nav_bar_container';
 import { Route } from 'react-router-dom';
 
 
 const App = () => (
   <div>
+    <Route path="/" component={NavbarContainer}/>
     <Route path='/signup' component={SignupContainer}/>
   </div>
 );
---
+++ frontend/components/navbar/nav_bar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default ({currentUser, logout}) => {
+  const display = currentUser ? 
+  (
+    <div>{currentUser.username}
+    <button onClick={this.logout}>Logout</button>
+    </div>
+  ) : 
+  (
+    <div>
+      <Link className="session-bttn" to="/signup">Sign Up</Link>
+      <Link className="session-bttn" to="/login">Log In</Link>
+    </div>
+  );
+
+
+  return (
+    <header className="navbar">
+      <h1 className="logo">soundstand</h1>
+      <div className="session-buttons">
+        {display}
+      </div>
+    </header>
+    
+  );
+}

530c928ddcefa6a019e2af36ec3993f88788100f
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-23 13:45:00 -0800

    Add ajax utils

---
+++ frontend/util/song_api_util.js
@@ -0,0 +1,37 @@
+export const addSong = (song) => {
+  $.ajax({
+    url: "api/songs",
+    method: "POST",
+    data: song
+  });
+};
+
+export const deleteSong = (id) => {
+  $.ajax({
+    url: `api/songs/${id}`,
+    method: "DELETE"
+  });
+};
+
+export const updateSong = (song) => {
+  $.ajax({
+    url: `api/songs/${song.id}`,
+    method: "PATCH",
+    data: song
+  });
+};
+
+export const getSong = (id) => {
+  $.ajax({
+    url: `api/songs/${id}`,
+    method: "GET"
+  });
+};
+
+export const getSongs = (id) => {
+  $.ajax({
+    url: `api/songs`,
+    method: "GET"
+  });
+};
+

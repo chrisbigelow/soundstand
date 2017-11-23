a12399c5ace0c9bee47d08d160ea6d40cc4c024a
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-23 11:34:02 -0800

    Add song controller

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -10,14 +10,33 @@ class Api::SongsController < ApplicationController
   end
 
   def update
-    @song = Chirp.find_by(params[:id])
+    @song = Song.find_by(params[:id])
     if @song && @song.update_attributes(song_params)
       render :show
+    elseif !@song
+      render json: ["Coudn't locate track"], status 400
     else
+      render json: @song.errors.full_messages, status: 401
     end
   end
 
+  def show
+    @song = Song.find_by(params[:id])
+  end
 
+  def index
+    @songs = Song.all
+  end
+
+  def destroy
+    @song = Song.find_by(params[:id])
+    if @song
+      @song.destroy
+      render :index
+    else
+      render json: ["Coudn't locate track"]
+    end
+  end
 
 private
 
